c36941e7ff5f7af24da1eb60031cc3b5af1c3ba9
Carsten Schneider <carsten.schneider@shopgate.com>
2018-01-15 16:00:37 +0100

    TD-707 preparations

--- product/streams/index.js
+++ product/streams/index.js
@@ -5,8 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import 'rxjs/add/operator/distinctUntilChanged';
-import 'rxjs/add/operator/switchMap';
 import { main$ } from '@shopgate/pwa-common/streams/main';
 import {
   RECEIVE_PRODUCT,
