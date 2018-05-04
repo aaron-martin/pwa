d2d4111355ba919c91a07f0d329e720c7fccadeb
Carina Hoffmann <carina.hoffmann@shopgate.com>
2018-05-04 13:12:24 +0200

    PWA-325: Implement schedulable widgets for PWA - adjust reducer to filter unpublished and not yet visible widgets

--- libraries/common/reducers/page/index.js
+++ libraries/common/reducers/page/index.js
@@ -11,15 +11,91 @@ import { persist } from '../../store/persistent';
 const STATE_VERSION = 'v1';
 
 /**
+ * Check the published and schedule states of the widget and perform
+ * necessary operations to hide the widget
+ * @param {Object} setting - widget settings
+ * @param {Date} nowDate - Current date object
+ * @return {boolean} hide widget or don't hide widget
+ */
+const shouldHideWidget = (setting, nowDate) => {
+  // Show widget if flag does not exist (old widgets)
+  if (!setting.hasOwnProperty('published')) {
+    return false;
+  }
+
+  if (!setting.published) {
+    return true;
+  }
+
+  if (setting.hasOwnProperty('plan') && setting.plan) {
+    let startDate = null;
+    let endDate = null;
+    let notStartedYet = false;
+    let finishedAlready = false;
+
+    if (setting.planDate.valid_from) {
+      startDate = new Date(setting.planDate.valid_from);
+      notStartedYet = nowDate <= startDate;
+    }
+
+    if (setting.planDate.valid_to) {
+      endDate = new Date(setting.planDate.valid_to);
+      finishedAlready = nowDate >= endDate;
+    }
+
+    // Don't hide if no dates found
+    if (!startDate && !endDate) {
+      return false;
+    }
+
+    // Hide if some wrong dates are passed
+    if (startDate && endDate && (startDate >= endDate)) {
+      return true;
+    }
+
+    // Hide if start date is set but it is not there yet
+    // Hide if end date is reached
+    if ((startDate && notStartedYet) || (endDate && finishedAlready)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+const filterWidgets = (widgets, nowDate) => {
+  return widgets.map((widget) => {
+    if (Array.isArray(widget.settings.widgets)) {
+      return {
+        ...widget,
+        settings: {
+          ...widget.settings,
+          widgets: filterWidgets(widget.settings.widgets),
+        },
+      };
+    }
+
+    if (!shouldHideWidget(widget.settings, nowDate)) {
+      return widget;
+    }
+
+    return null;
+  }).filter(w => !!w);
+}
+
+/**
  * Enrich the widget data.
  * @param {Object} action The action that was received.
  * @return {Object} An enriched set of widgets.
  */
-const enrichWidgets = action =>
-  action.config.widgets.map((widget, index) => ({
+const processWidgets = (action) => {
+  const widgets = action.config.widgets.map((widget, index) => ({
     ...widget,
     id: `${action.pageId}-${index}-${widget.type}`,
   }));
+
+  return filterWidgets(widgets, new Date());
+}
 
 /**
  * The page config reducer.
@@ -43,7 +119,7 @@ const page = (state = {}, action) => {
         ...state,
         [action.pageId]: {
           title: action.config.title,
-          widgets: enrichWidgets(action),
+          widgets: processWidgets(action),
           isFetching: false,
           expires: Date.now(),
         },
